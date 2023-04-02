981d3d2865ee512a224f32c53c8845f84fb8c9e0
kRamani28 <115987763+kRamani28@users.noreply.github.com>
2023-04-02 02:27:08 -0700

    Add files via upload

---
+++ Challenge5.py
@@ -0,0 +1,34 @@
+# 5. Handling a Bank Account
+class Account:
+    def __init__(self, title=None, balance=0):
+        self.title = title
+        self.balance = balance
+    def withdrawal(self, amount):
+        self.balance -= amount
+    def deposit(self, amount):
+        self.balance += amount
+    def getBalance(self):
+        return self.balance
+    
+class SavingsAccount(Account):
+    def __init__(self, title=None, balance=0, interestRate=0):
+        super().__init__(title, balance)
+        self.interestRate = interestRate
+    def interestAmount(self):
+        return(self.balance * self.interestRate)/100
+demo1 = SavingsAccount("Ashish", 2000, 5)
+demo1.deposit(500)
+print(demo1.getBalance())
+
+demo1 = SavingsAccount("Ashish", 2000, 5)
+demo1.withdrawal(500)
+print(demo1.getBalance())
+
+demo1 = SavingsAccount("Ashish", 2000, 5)
+print(demo1.interestAmount())
+
+# output
+
+2500
+1500
+100.0
